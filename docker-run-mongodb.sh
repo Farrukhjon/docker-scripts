dcd2c5a7019bc0050e71e424224e2da21ac71ce9
Farrukh Sattorov <firedandy@gmail.com>
2016-01-11 11:19:46 +0500

    improvied so that it now supports upgrading

--- docker-run-mongodb.sh
+++ docker-run-mongodb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker pull mongo
 docker stop mongodb
 docker rm mongodb
 docker run --detach --name mongodb --hostname mongodb --publish 127.0.0.1:27017:27017 --restart always --volume mongo-data:/var/lib/mongo-data mongo

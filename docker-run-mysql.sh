744dd84d9f5143dca67e4f4bdbdd778293c4dd39
Farrukh Sattorov <firedandy@gmail.com>
2016-02-15 11:12:12 +0500

    externilizing configuration

--- docker-run-mysql.sh
+++ docker-run-mysql.sh
@@ -4,6 +4,6 @@ docker pull mysql
 docker stop mysql
 docker rm mysql
 
-docker run --detach --name mysql --hostname mysql --publish 127.0.0.1:3306:3306 --restart always \
+docker run --detach --name mysql --volume /etc/mysql/conf.d:/etc/mysql/conf.d --hostname mysql --publish 127.0.0.1:3306:3306 --restart always \
            --env MYSQL_ROOT_PASSWORD=sa --env MYSQL_USER=mysql --env MYSQL_PASSWORD=mysql --env MYSQL_DATABASE=testdb --volume mysql-data:/var/lib/mysql-data mysql
 

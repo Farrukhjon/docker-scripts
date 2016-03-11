f61db8b86b62f5ee5b6a57fda20c080ebc657b83
Farrukh Sattorov <firedandy@gmail.com>
2016-03-11 15:30:32 +0500

    updated

---
+++ docker-run-postgres.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker pull postgres
+docker stop postgres
+docker rm postgres
+
+docker run --detach --name postgres --hostname postgres --publish 127.0.0.1:5432:5432 --restart always \
+           --env POSTGRES_PASSWORD=sa --env POSTGRES_USER=postgres --env POSTGRES_DB=testdb --volume postgres-data:/var/lib/postgres-data postgres
+
+
--- docker-run-tomcat.sh
+++ docker-run-tomcat.sh
@@ -4,4 +4,4 @@ docker pull tomcat:8-jre8
 docker stop tomcat
 docker rm tomcat
 docker run --detach --name tomcat --hostname tomcat --publish 127.0.0.1:8888:8080 --restart always \
-                --volume $HOME/Docker/tomcat/tomcat-users.xml:/usr/local/tomcat/conf/tomcat-users.xml:ro tomcat:8-jre8
+                --volume /etc/tomcat/tomcat-users.xml:/usr/local/tomcat/conf/tomcat-users.xml:ro tomcat:8-jre8

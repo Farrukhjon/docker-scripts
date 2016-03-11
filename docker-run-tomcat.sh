f5ba3aacf5c4d2f559b91180cfae1f1a028b4f35
Farrukh Sattorov <firedandy@gmail.com>
2016-03-11 18:08:50 +0500

    updated tomcat container run

--- docker-run-tomcat.sh
+++ docker-run-tomcat.sh
@@ -4,4 +4,7 @@ docker pull tomcat:8-jre8
 docker stop tomcat
 docker rm tomcat
 docker run --detach --name tomcat --hostname tomcat --publish 127.0.0.1:8888:8080 --restart always \
-                --volume /etc/tomcat/tomcat-users.xml:/usr/local/tomcat/conf/tomcat-users.xml:ro tomcat:8-jre8
+           --link postgres:postgres \
+           --volume /etc/tomcat/tomcat-users.xml:/usr/local/tomcat/conf/tomcat-users.xml:ro \
+           --volume /etc/tomcat/context.xml:/usr/local/tomcat/conf/context.xml:ro \
+           tomcat:8-jre8
